5f248359e06063caec4d262c8c6b7b2540335106
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-09-15 22:02:54 +0200

    V4.3 use optimized browser server bootstrap

--- URUCHOM_W_PRZEGLADARCE.bat
+++ URUCHOM_W_PRZEGLADARCE.bat
@@ -6,7 +6,7 @@ set "PYTHON_EXE=C:\Python314\python.exe"
 if not exist "%PYTHON_EXE%" set "PYTHON_EXE=python"
 
 rem The desktop app also uses port 8000. Close it before running this launcher.
-start "Archivebate Browser Server" cmd /k ""%PYTHON_EXE%" -m uvicorn main:app --host 127.0.0.1 --port 8000"
+start "Archivebate Browser Server" cmd /k ""%PYTHON_EXE%" browser_server.py"
 
 timeout /t 2 /nobreak >nul
 start "" "http://127.0.0.1:8000/"
